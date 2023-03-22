c126649ecb72095a288fa114c3f51ad2731397c8
Manoel Ribeiro <manoel.ribeiro@unilab.edu.br>
2023-03-22 18:57:13 -0300

    splash

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,11 +7,15 @@ version="0.9.0-beta"
 build=$(date +%Y%m%d)
 git tag  $version 
 git push origin --tags
+
+pkill -f 'k2'
 go build  -ldflags "-X 'github.com/antoniomralmeida/k2/pkg/version.Version=$version' -X 'github.com/antoniomralmeida/k2/pkg/version.Build=$build' " -o ./bin/k2web.bin ./cmd/k2web/main.go
 go build  -ldflags "-X 'github.com/antoniomralmeida/k2/pkg/version.Version=$version' -X 'github.com/antoniomralmeida/k2/pkg/version.Build=$build' " -o ./bin/k2olivia.bin ./cmd/k2olivia/main.go
 go build  -ldflags "-X 'github.com/antoniomralmeida/k2/pkg/version.Version=$version' -X 'github.com/antoniomralmeida/k2/pkg/version.Build=$build' " -o ./bin/k2.bin ./cmd/k2/main.go
 
-gnome-terminal -- ./bin/k2olivia.bin &
+konsole -e ./bin/k2olivia.bin & 
 sleep 10
-gnome-terminal -- ./bin/k2web.bin &
-gnome-terminal -- ./bin/k2.bin &
+konsole -e ./bin/k2web.bin & 
+konsole -e ./bin/k2.bin  
+
+pkill -f 'k2'
